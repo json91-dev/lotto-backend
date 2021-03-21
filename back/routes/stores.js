44761c3bc05d3cd66cf20a64f70243df99d36af5
jungwoole91 <jungwoolee91@gmail.com>
2021-03-22 02:34:22 +0900

    store 반경 검색 rawQuery => 시퀄라이즈 쿼리로 전환

--- back/routes/stores.js
+++ back/routes/stores.js
@@ -55,4 +55,51 @@ router.get('/searchRadius', async (req, res, next) => { // POST /api/store
   }
 });
 
+router.get('/searchRadiusEdit', async (req, res, next) => { // POST /api/store
+
+  if (!req.query['long']) {
+    res.status(400).send('경도(long) 값이 존재하지 않습니다.');
+  }
+
+  if (!req.query['lat']) {
+    res.status(400).send('위도(lat) 값이 존재하지 않습니다.');
+  }
+
+  if (!req.query['rad']) {
+    res.status(400).send('반경(rad) 값이 존재하지 않습니다.');
+  }
+
+  if (isNaN(req.query['long']) || isNaN(req.query['lat'] || isNaN(req.query['rad']))) { // 숫자가 아니라면
+    res.status(400).send('값이 없거나 유효하지 않은 타입의 값을 입력하였습니다.')
+  }
+
+  const { lat, long, rad } = req.query;
+
+  try {
+    const stores = await db.Store.findAll({
+      attributes: [
+        [ db.sequelize.literal(`6371*acos(cos(radians(${lat}))*cos(radians(latitude))*cos(radians(longitude) -radians(${long}))+sin(radians(${lat}))*sin(radians(latitude)))`), 'distance' ],
+        'address',
+        'address_new',
+        'name',
+        'phone',
+        'region1',
+        'region2',
+        'storetype',
+        'latitude',
+        'longitude',
+      ],
+      having: db.sequelize.literal(`distance < ${rad}`),
+      order: db.sequelize.literal(`distance ASC`),
+    });
+
+    res.status(200).json(stores);
+
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+
+});
+
 module.exports = router;
